112ab8c223aeba7835f5d7ba0b19bd14c379077a
venkatesh21m <46576190+venkatesh21m@users.noreply.github.com>
2021-03-21 16:00:02 +0000

    with prototype model base

--- TowerDefence/Assets/Scripts/Engine/Managers/WaveManager.cs
+++ TowerDefence/Assets/Scripts/Engine/Managers/WaveManager.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveManager : MonoBehaviour
+namespace Rudrac.TowerDefence.Managers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class WaveManager : MonoBehaviour
     {
-        
+        public static WaveManager instance;
+
+        public Wave[] Wave;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            instance = this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    [System.Serializable]
+    public class Wave
     {
-        
+        public GameObject[] enemies;
+        public int numberofenemies;
     }
+
 }
